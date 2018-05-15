387351f3c328b7699ed3d390621c3873db7a11aa
ki3wi <shan_5210@126.com>
2018-05-15 18:36:36 +0800

    add util-keyboard and util-clipboard

---
+++ KeyWordsFrameWork/util/KeyBoardUtil.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import win32api
+import win32con
+
+class KeyBoardKeys(object):
+    '''
+    模拟键盘按键类
+    '''
+    VK_CODE = {
+        'enter': 0x0D,
+        'ctrl': 0x11,
+        'v': 0x56}
+
+    @staticmethod
+    def keyDown(keyNmae):
+        #按下按键
+        win32api.keybd_event(KeyBoardKeys.VK_CODE[keyNmae],0,0,0)
+
+    @staticmethod
+    def keyUp(keyName):
+        #释放按键
+        win32api.keybd_event(KeyBoardKeys.VK_CODE[keyName],0,win32con.KEYEVENTF_KEYUP,0)
+
+    @staticmethod
+    def oneKey(key):
+        #模拟单个按键
+        KeyBoardKeys.keyDown(key)
+        KeyBoardKeys.keyUp(key)
+
+    @staticmethod
+    def twoKeys(key1,key2):
+        #模拟两个组合按键
+        KeyBoardKeys.keyDown(key1)
+        KeyBoardKeys.keyDown(key2)
+        KeyBoardKeys.keyUp(key1)
+        KeyBoardKeys.keyUp(key2)
---
+++ decorated.py
@@ -0,0 +1,1 @@
+__author__ = 'ki3wi'
